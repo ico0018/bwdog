77a8bc8aa698e69bbbb47c26a38eff11c422a574
jackchanel <1667716062@qq.com>
2024-08-27 17:52:34 +0800

    temp: add willuse page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import withTabbar from "./components/withTabbar";
 
 import Index from "./pages/Index";
+import Leaderboard from "./pages/Leaderboard";
+import Friends from "./pages/Friends";
 import Welcome from "./pages/WelcomeWizard";
-import withTabbar from "./components/withTabbar";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" Component={withTabbar(Index)}></Route>
+        <Route path="/leaderboard" Component={withTabbar(Leaderboard)}></Route>
+        <Route path="/friends" Component={withTabbar(Friends)}></Route>
         <Route path="/welcome" Component={Welcome}></Route>
         <Route path="*" element={<h1>Not Found</h1>}></Route>
       </Routes>
--- src/components/CowTabbar.jsx
+++ src/components/CowTabbar.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-
+import React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import LeaderboardRoundedIcon from "@mui/icons-material/LeaderboardRounded";
 import GroupRoundedIcon from "@mui/icons-material/GroupRounded";
+import { useNavigate } from "react-router-dom";
 
 export default () => {
   const tabs = [
@@ -12,23 +12,31 @@ export default () => {
       key: "Home",
       title: "Home",
       icon: <HomeRoundedIcon />,
+      path: "/",
     },
     {
       key: "Rank",
       title: "Leaderboard",
       icon: <LeaderboardRoundedIcon />,
+      path: "/leaderboard",
     },
     {
       key: "Friend",
       title: "Friends",
       icon: <GroupRoundedIcon />,
+      path: "/friends",
     },
   ];
-  const [value, setValue] = React.useState(0);
 
+  const [value, setValue] = React.useState(0);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const router = useNavigate();
+  function navigateTo(path) {
+    router(path);
+  }
 
   return (
     <div className="fixed bottom-0 w-full backdrop-blur-sm text-white">
@@ -42,12 +50,15 @@ export default () => {
           <Tab
             key={item.key}
             disableRipple
+            disableFocusRipple
+            disableTouchRipple
             sx={{
               color: value === index ? "" : "#A6A6A6",
               textTransform: "lowercase",
             }}
             icon={item.icon}
             label={item.title}
+            onClick={() => navigateTo(item.path)}
           />
         ))}
       </Tabs>
